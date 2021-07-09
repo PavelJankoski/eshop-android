61e45f2d600ee521ed1f799a7caa77488a69e8e0
Pavel Jankoski <paveljankoski@hotmail.com>
2021-07-09 16:59:04 +0200

    Facebook oauth2 login setup

--- app/src/main/java/mk/ukim/finki/eshop/MyApplication.kt
+++ app/src/main/java/mk/ukim/finki/eshop/MyApplication.kt
@@ -1,8 +1,14 @@
 package mk.ukim.finki.eshop
 
 import android.app.Application
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppEventsLogger.activateApp(this);
+    }
 }
--- app/src/main/java/mk/ukim/finki/eshop/ui/account/AccountFragment.kt
+++ app/src/main/java/mk/ukim/finki/eshop/ui/account/AccountFragment.kt
@@ -1,17 +1,23 @@
 package mk.ukim.finki.eshop.ui.account
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import mk.ukim.finki.eshop.R
+import androidx.fragment.app.Fragment
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import mk.ukim.finki.eshop.databinding.FragmentAccountBinding
 
 
 class AccountFragment : Fragment() {
     private var _binding: FragmentAccountBinding? = null
     private val binding get() = _binding!!
+    lateinit var callbackManager: CallbackManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,7 +25,43 @@ class AccountFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        callbackManager =  CallbackManager.Factory.create();
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val EMAIL = "email"
+
+        binding.loginButton.setReadPermissions(listOf(EMAIL))
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                // App code
+                val asd = ""
+            }
+
+            override fun onCancel() {
+                // App code
+                val asd = ""
+
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+                val asd = ""
+            }
+        })
+
+
         return binding.root
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onDestroyView() {
