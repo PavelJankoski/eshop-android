834b41b831f09e3ab509ddc62e0c5533e13c97dd
Pavel Jankoski <paveljankoski@hotmail.com>
2021-07-10 19:44:34 +0200

    Refactored facebook login

--- app/src/main/java/mk/ukim/finki/eshop/ui/account/AccountFragment.kt
+++ app/src/main/java/mk/ukim/finki/eshop/ui/account/AccountFragment.kt
@@ -2,14 +2,13 @@ package mk.ukim.finki.eshop.ui.account
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
+import com.facebook.*
+import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import mk.ukim.finki.eshop.databinding.FragmentAccountBinding
 
@@ -26,33 +25,22 @@ class AccountFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         callbackManager =  CallbackManager.Factory.create();
-        val accessToken = AccessToken.getCurrentAccessToken()
-        val EMAIL = "email"
 
-        binding.loginButton.setReadPermissions(listOf(EMAIL))
-        // If you are using in a fragment, call loginButton.setFragment(this);
+        binding.loginButton.fragment = this
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {
+                    Log.i("login", "success")
+                }
 
-        // Callback registration
-        // If you are using in a fragment, call loginButton.setFragment(this);
+                override fun onCancel() {
+                    Log.i("login", "cancel")
+                }
 
-        // Callback registration
-        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
-            override fun onSuccess(loginResult: LoginResult?) {
-                // App code
-                val asd = ""
-            }
-
-            override fun onCancel() {
-                // App code
-                val asd = ""
-
-            }
-
-            override fun onError(exception: FacebookException) {
-                // App code
-                val asd = ""
-            }
-        })
+                override fun onError(e: FacebookException) {
+                    Log.i("login", "error")
+                }
+            })
 
 
         return binding.root
@@ -60,8 +48,9 @@ class AccountFragment : Fragment() {
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
     }
 
     override fun onDestroyView() {
